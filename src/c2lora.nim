d5f4316f2cb6e24135a9525597ae263e16e1f6b6
Joshua Zimmerman <joshua.curtis.zimmerman@gmail.com>
2025-11-22 23:57:04 -0600

    Potential Implementation for LED blinky

--- src/c2lora.nim
+++ src/c2lora.nim
@@ -1,10 +1,27 @@
-{.warning[UnusedImport]: off.}
 import startup
-{.warning[UnusedImport]: on.}
+import p
+
+proc delay(t: int) =
+  var n = t
+  while n > 0:
+    dec n
+    var x {.volatile.} = 500
+    while x > 0:
+      dec x
+
+proc main() =
+  const ledPinNum = 4 # FIXME
+  const ledPinBit = 1'u32 shl ledPinNum
+  P0.DIR = ledPinBit # might need bit-invert depending on meaning of 0/1
+  while true:
+    # Turn LED on
+    P0.OUTSET = ledPinBit
+    delay(1000)
+    # Turn LED off
+    P0.OUTCLR = ledPinBit
+    delay(1000)
 
 proc entry() {.exportc: "entry".} =
-  var s = @[1,2,3]
-  s.add(5)
-  # Main loop
+  main()
   while true:
     discard
