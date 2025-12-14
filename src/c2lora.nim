93762f6fd3eac09e7ab99b1a10b0d9a85b76d1e6
!!Dean <dwhall256@gmail.com>
2025-12-13 23:48:26 -0600

    WIP: Moving toward event-driven blinky
    Implemented configureSystick100ms in nim.
    
    Moved blocking-wait blink to fault_Handler so any
    hard fault will blink green IPSR number of times.

--- src/c2lora.nim
+++ src/c2lora.nim
@@ -1,3 +1,4 @@
+import cm4f/systick
 import nrf52840/p
 import reset
 
@@ -7,17 +8,10 @@ const
   bluePinBit = 1'u32 shl 4 # P1.04/LED2/RAK19007 Blue
   greenPinBit = 1'u32 shl 3 # P1.03/LED1/RAK19007 Green
 
-var blink {.volatile.}: bool = false
-
 proc readIpsr(): uint32 {.inline.} =
   asm """
     mrs %0, ipsr
     : "=r"(`result`)
-  """
-
-proc waitForInterrupt() {.inline.} =
-  asm """
-    wfi
   """
 
 func waitBlocking(ticks: int) =
@@ -34,23 +28,53 @@ proc blinkLed(pinBit: static uint32, delay: int) =
   P1.OUTCLR = pinBit
   waitBlocking(delay)
 
-proc default_Handler() {.exportc, noconv.} =
+proc fault_Handler() {.exportc, noconv.} =
   ## Blinks green LED based on IPSR value
+  # Fault handlers are higher priority than SysTick
+  # which prevents SysTick from interrupting this handler;
+  # so we must use blocking waits to flash the LED
   let ipsr = readIpsr()
-  waitForInterrupt()
+  P1.DIRSET = greenPinBit
+  while true:
+    for i in 0 ..< ipsr:
+      blinkLed(greenPinBit, standardDelay)
+    waitBlocking(longDelay)
+
+proc default_Handler() {.exportc, noconv.} =
+  # TODO: clear interrupt
+  discard
+
+proc waitForInterrupt() {.inline.} =
+  asm """
+    wfi
+  """
+
+proc configureSystick100ms() =
+  SYSTICK.SYST_CSR = 0'u32
+  var cal = SYSTICK.SYST_CALIB.TENMS.uint32
+  if cal == 0'u32:
+    cal = 6_400_000'u32 - 1'u32
+  SYSTICK.SYST_RVR = cal
+  SYSTICK.SYST_CVR = 0'u32
+  SYSTICK.SYST_CSR
+         .CLKSOURCE(1)
+         .TICKINT(1)
+         .ENABLE(1)
+         .write()
 
 proc SysTick_Handler() {.exportc, noconv.} =
-  P1.DIRSET = bluePinBit
-  blink = not blink;
-  if blink:
+  var ledState {.global, volatile.} = false
+  ledState = not ledState
+  if ledState:
     P1.OUTSET = bluePinBit
   else:
     P1.OUTCLR = bluePinBit
 
-  waitForInterrupt()
-
 proc main() =
-  waitForInterrupt()
+  P1.DIRSET = bluePinBit
+  configureSystick100ms()
+  while true:
+    waitForInterrupt()
 
 when isMainModule:
   main()
