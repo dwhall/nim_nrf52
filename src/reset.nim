2962b529aec1524b8b1e1c2c199c4ca4b4ac5394
Joshua Zimmerman <joshua.curtis.zimmerman@gmail.com>
2025-12-22 22:04:05 -0600

    Init VTOR for working event based blinky

--- src/reset.nim
+++ src/reset.nim
@@ -3,11 +3,14 @@
 {.compile: "std.c".}
 {.compile: "section_init.c".}
 
+import cm4f/scb
+
 proc NimMain() {.importc: "NimMain".}
 proc copyDataSection() {.importc, cdecl.}
 proc zeroBssSection() {.importc, cdecl.}
 
 proc Reset_Handler() {.exportc, noconv.} =
+  SCB.VTOR = 0x00026000
   copyDataSection()
   zeroBssSection()
   NimMain() # this will call the nim module given to the nim compiler
