1a54d15733eb3eb46c38a4856b950074c7edbed7
!!Dean <dwhall256@gmail.com>
2025-12-26 23:11:49 -0600

    Access core special registers

--- src/hard_fault.nim
+++ src/hard_fault.nim
@@ -1,4 +1,5 @@
 import nrf52840/p
+import cm4f/core
 
 # The fault_Handler overrides the weak symbol in the vector table.
 # Any hard fault will trigger this handler, despite no reference
@@ -9,12 +10,6 @@ const
   standardDelay = 1000
   longDelay = 5000
   greenPinBit = 1'u32 shl 3 # P1.03/LED1/RAK19007 Green
-
-proc readIpsr(): uint32 {.inline.} =
-  asm """
-    mrs %0, ipsr
-    : "=r"(`result`)
-  """
 
 func waitBlocking(ticks: int) =
   var outerTicks = ticks
@@ -31,14 +26,15 @@ proc blinkLed(pinBit: static uint32, delay: int) =
   waitBlocking(delay)
 
 proc fault_Handler() {.exportc, noconv, noreturn.} =
-  ## Blinks green LED based on IPSR value
-  # Fault handlers are higher priority than SysTick
+  ## Blinks the green LED ISR number of times.
+  ## Repeats the count after a noticeable pause.
+  # Fault handler exceptions are higher priority than SysTick
   # which prevents SysTick from interrupting this handler;
   # so we must use blocking waits to flash the LED
-  let ipsr = readIpsr()
+  let blinkCount = IPSR.ISR_NUMBER
   P1.DIRSET = greenPinBit
   while true:
-    for i in 0 ..< ipsr:
+    for i in 0 ..< blinkCount:
       blinkLed(greenPinBit, standardDelay)
     waitBlocking(longDelay)
 
