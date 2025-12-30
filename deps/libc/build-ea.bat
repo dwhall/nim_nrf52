8e3425fa607ebd6c04374daeada99935773c9b4c
!!Dean <dwhall256@gmail.com>
2025-12-30 11:17:25 -0600

    Remove embedded artistry submodule

--- deps/libc/build-ea.bat
+++
@@ -1,7 +0,0 @@
-cd embeddedartistry
-git submodule update --init --recursive
-rmdir /S /Q build
-mkdir build
-cd build
-cmake .. -G Ninja -DCMAKE_TOOLCHAIN_FILE="../ea-toolchain.cmake" DLIBC_BUILD_MATH=ON
-ninja
